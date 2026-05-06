09f1f8eea3c0b5fb2abb1ec378c136bc7ca05e4b
Quincy <fayetendilla@gmail.com>
2026-05-07 04:18:54 +0800

    working forest level

--- Assets/Scripts/Statue.cs
+++ Assets/Scripts/Statue.cs
@@ -2,16 +2,28 @@ using UnityEngine;
 
 public class Statue : MonoBehaviour, IInteractable
 {
-    public bool isFixed = false;
-    
+    [SerializeField] GameObject statue;
+    [SerializeField] bool isFixed = false;
+
+    private void Awake()
+    {
+        statue.SetActive(isFixed);
+    }
+
     public void Interact()
     {
-        if (isFixed)
-            Debug.Log("Statue is already fixed.");
-        else
+        if (!isFixed)
         {
+            statue.SetActive(true);
+
             Debug.Log("Statue fixed.");
             isFixed = true;
+
+            GetComponent<Statue>().enabled = false;
+        }
+        else
+        {
+            Debug.Log("Statue is already fixed.");
         }
     }
 }
---
+++ Assets/Scripts/Totem.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Totem : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        // action here
+    }
+}
