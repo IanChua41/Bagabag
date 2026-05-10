0c48d69172d1c7bff93495ad35698f91e704da02
Quincy <fayetendilla@gmail.com>
2026-05-11 02:56:53 +0800

    Revert "with cutscene to level3"
    This reverts commit 743f691e91fe6179e810a35c5fa2f196693203ef.

--- Assets/Scripts/Totem.cs
+++ Assets/Scripts/Totem.cs
@@ -1,39 +1,11 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.Video;
-using UnityEngine.SceneManagement;
 
 public class Totem : MonoBehaviour, IInteractable
 {
-    [Header("Video Settings")]
-    [SerializeField] private VideoClip videoClip;
-    [SerializeField] private VideoPlayer videoPlayer;
-
     public void Interact()
     {
-        Debug.Log("Playing cutscene...");
-        StartCoroutine(PlayCutscene());
-    }
+        // action here
 
-    private IEnumerator PlayCutscene()
-    {
-        // Play video
-        if (videoPlayer != null && videoClip != null)
-        {
-            Debug.Log("Playing video: " + videoClip.name);
-            videoPlayer.clip = videoClip;
-            videoPlayer.Play();
-            
-            // Wait for video to finish
-            while (videoPlayer.isPlaying)
-            {
-                yield return null;
-            }
-            Debug.Log("Video finished");
-        }
-
-        // Load Level2
         Debug.Log("Level finished.");
-        SceneManager.LoadScene(4);
     }
 }
