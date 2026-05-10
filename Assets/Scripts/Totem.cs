7c908907881b19ab2138247a7f1c13659f36b8e7
Quincy <fayetendilla@gmail.com>
2026-05-11 03:08:22 +0800

    cutscene

--- Assets/Scripts/Totem.cs
+++ Assets/Scripts/Totem.cs
@@ -1,12 +1,48 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
 public class Totem : MonoBehaviour, IInteractable
 {
+    [Header("Video Settings")]
+    [SerializeField] private VideoClip videoClip;
+    [SerializeField] private VideoPlayer videoPlayer;
+
     public void Interact()
     {
-        Debug.Log("Level finished.");
+        Debug.Log("Playing cutscene...");
+        StartCoroutine(PlayCutscene());
+    }
 
-        SceneManager.LoadScene("Level3");
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("X key pressed, triggering cutscene...");
+            StartCoroutine(PlayCutscene());
+        }
+    }
+
+    private IEnumerator PlayCutscene()
+    {
+        // Play video
+        if (videoPlayer != null && videoClip != null)
+        {
+            Debug.Log("Playing video: " + videoClip.name);
+            videoPlayer.clip = videoClip;
+            videoPlayer.Play();
+
+            // Wait for video to finish
+            while (videoPlayer.isPlaying)
+            {
+                yield return null;
+            }
+            Debug.Log("Video finished");
+        }
+
+        // Load Level2
+        Debug.Log("Level finished.");
+        SceneManager.LoadScene(4);
     }
 }
